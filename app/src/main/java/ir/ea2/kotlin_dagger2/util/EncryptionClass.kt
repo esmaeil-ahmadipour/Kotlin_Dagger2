41c41d6e0729f58876927772bf60edfea607c9a2
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-26 12:11:44 +0330

    Commit> Add Method Injection

--- app/src/main/java/ir/ea2/kotlin_dagger2/util/EncryptionClass.kt
+++ app/src/main/java/ir/ea2/kotlin_dagger2/util/EncryptionClass.kt
@@ -5,18 +5,23 @@ import ir.ea2.kotlin_dagger2.di.EncryptionSecurity
 import javax.inject.Inject
 
 class EncryptionClass @Inject constructor() {
-    @Inject
-    lateinit var security:EncryptionSecurity
+
+    var security: EncryptionSecurity? =null
 
     fun encrypt(stringValue:String):String{
         Log.i("TAG_ENCRYPT:","Run EncryptionClass.encrypt()")
-        Log.i("TAG_KEY:","Get SECURITY_KEY : ${security.SECURITY_KEY}")
+        Log.i("TAG_KEY:","Get SECURITY_KEY : ${security?.SECURITY_KEY}")
 
         return stringValue
     }
     fun decrypt(stringValue:String):String{
         Log.i("TAG_DECRYPT:","Run EncryptionClass.decrypt()")
-        Log.i("TAG_IV:","Get SECURITY_IV : ${security.SECURITY_IV}")
+        Log.i("TAG_IV:","Get SECURITY_IV : ${security?.SECURITY_IV}")
         return stringValue
+    }
+
+    @Inject
+    fun encryptionSecurityInject(security:EncryptionSecurity){
+        this.security=security
     }
 }
