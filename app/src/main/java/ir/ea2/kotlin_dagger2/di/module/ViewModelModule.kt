ca6a98ddc11bb11e2363b32d1615bcfef4942a43
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-02 23:01:33 +0330

    Commit> Use @Binds Annotation In ViewModelModule (Replace To @Providers Annotation)

--- app/src/main/java/ir/ea2/kotlin_dagger2/di/module/ViewModelModule.kt
+++ app/src/main/java/ir/ea2/kotlin_dagger2/di/module/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package ir.ea2.kotlin_dagger2.di.module
 
 import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
@@ -9,20 +10,18 @@ import ir.ea2.kotlin_dagger2.view_models.MainActivityViewModel
 import ir.ea2.kotlin_dagger2.view_models.SecondActivityViewModel
 
 @Module
-class ViewModelModule {
+abstract class ViewModelModule {
 
-    @Provides
+    @Binds
     @ViewModelKey(MainActivityViewModel::class)
     @IntoMap
-    fun provideMainViewModule(vm:MainActivityViewModel) : ViewModel{
-        return vm
-    }
+    abstract fun provideMainViewModule(vm:MainActivityViewModel) : ViewModel
 
 
-    @Provides
+
+    @Binds
     @ViewModelKey(SecondActivityViewModel::class)
     @IntoMap
-    fun provideSecondViewModule(vm:SecondActivityViewModel):ViewModel{
-        return vm
-    }
+   abstract fun provideSecondViewModule(vm:SecondActivityViewModel):ViewModel
+
 }
